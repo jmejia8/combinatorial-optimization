34d706d1c95959b6901ecb81fa9f567c13ef8892
Jesús Mejía <jesusmejded@gmail.com>
2018-09-05 09:02:12 -0500

    Bins

--- structures.jl
+++ structures.jl
@@ -2,3 +2,15 @@ mutable struct Permutation
     w::Array{Real}
     f::Int32
 end
+
+mutable struct Bin
+    x::Array{Int}   # items
+    w::Array{Real}  # whight
+    C::Real         # capacity
+    f::Int32        # obj. func. value
+end
+
+struct BinPacking
+    w::Array{Real}
+    C::Real
+end
