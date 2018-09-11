ec7cbce8dd77154c76ad2696ef86f08971b0d47f
Jesús Mejía <jesusmejded@gmail.com>
2018-09-11 13:32:50 -0500

    commoin functions

---
+++ tools.jl
@@ -0,0 +1,23 @@
+import Statistics.std, Statistics.mean
+import Printf.@printf
+
+function printbin(b::Bin)
+    println("x: ", b.x)
+    println("w: ", b.w)
+    println("C = $(b.C) \t f = $(b.f)")
+end
+
+function printbin(bins::Array{Bin})
+    i = 1
+    for bin ∈ bins
+        println("================= Bin $i =================")
+        printbin(bin)
+        # println("===========================================")
+        i += 1
+    end
+end
+
+function summary(bins::Array{Bin})
+    Cs = [ bin.f for bin ∈ bins]
+    @printf("|B| = %d \t mean(w) = %.3f \t std(w) = %.3f\n", length(bins), mean(Cs), std(Cs))
+end
